02a5fa364f052d25e6e8865d659a503258a8626c
redstone2010 <31410740+redstone2010@users.noreply.github.com>
2017-11-04 17:41:07 -0400

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,6 +1,18 @@
+// Pi
 function pi(){
-  math.PI();
+  return math.PI();
 }
+
+// Exponents
 function pow(num, exp){
   return num ** exp;
+}
+
+// Initialization
+function init(json/* json is either true/false */, light/* light it either true/false */){
+ var options = {
+	_json: json,
+	_fast: light
+ };
+
 }
